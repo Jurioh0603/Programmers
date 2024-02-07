628fb4dfc4a3139251919f3152aafa05050f9cec
Jurioh0603 <148063470+Jurioh0603@users.noreply.github.com>
2024-02-07 13:42:59 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 14.27 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String front = my_string.substring(0,s);
+        String back = my_string.substring(s+overwrite_string.length());
+        String answer = "";
+        answer = front + overwrite_string + back;
+        return answer;
+    }
+}
