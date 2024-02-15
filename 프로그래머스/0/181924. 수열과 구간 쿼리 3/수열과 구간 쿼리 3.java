7a53f2bdf9208c1a45bdaa8c4537cd003db1d162
Jurioh0603 <148063470+Jurioh0603@users.noreply.github.com>
2024-02-15 09:54:56 +0900

    Update 수열과 구간 쿼리 3.java

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -3,11 +3,11 @@ class Solution {
         int[] answer = {};
         int iValue = 0;
         for(int i=0; i<queries.length; i++) {
-        	iValue = arr[queries[i][0]];
-        	arr[queries[i][0]] = arr[queries[i][1]];
+        	iValue = arr[queries[i][0]]; //임시 값 넣기
+        	arr[queries[i][0]] = arr[queries[i][1]];  //값 교환
         	arr[queries[i][1]] = iValue;
         }
-        for(int i=0; i<arr.length; i++) {
+        for(int i=0; i<arr.length; i++) { //배열 복사
         	answer = arr;
         }
         return answer;
