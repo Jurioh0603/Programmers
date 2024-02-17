aa1efa8403b0ab4e15e645940eed831cad73a6d0
Jurioh0603 <148063470+Jurioh0603@users.noreply.github.com>
2024-02-18 00:25:41 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 67.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int count = end_num - start_num+1;
+        int[] answer = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            answer[i] = start_num + i;
+        }
+        return answer;
+    }
+}
